81f9248e300334cb05c572e01bb44da6f57f3184
Tiber <trantien.developer@gmail.com>
2024-03-11 19:58:39 +0700

    convert string to int

--- Assets/ScriptableObjects/SkillStatsConfig/SObjSkillStatsConfig.cs
+++ Assets/ScriptableObjects/SkillStatsConfig/SObjSkillStatsConfig.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 public class SObjSkillStatsConfig : ScriptableObject
 {
     public string skillName;
+    public string describe_1;
+    public string describe_2;
     public int cooldown;
-    public int levelMax = 100;
+    public int levelMax = 10;
     public int totalPointByXLv = 6; // total point is define by multipe current level
-    public string damage, ownedEffect; // next value = current value + level (maybe change later)
+    public int damage, ownedEffect; // next value = current value + level (maybe change later)
     public Sprite skillSpt;
+    public Skill prefab;
 }
