46c6921486740debcff09f71adb11e00e9c6914b
Tiber <trantien.developer@gmail.com>
2024-03-23 23:44:44 +0700

    create effect in general stat button

--- Assets/Scripts/UI/HoldGStatsButton.cs
+++ Assets/Scripts/UI/HoldGStatsButton.cs
@@ -6,17 +6,22 @@ public class HoldGStatsButton : MonoBehaviour, IPointerDownHandler, IPointerUpHa
 {
     [SerializeField] private GeneralItem generalItem;
     [SerializeField] private Image imgBtn;
+    [SerializeField] private RectTransform rectTfm;
     [SerializeField] private GameObject maxLvGO;
 
-    [SerializeField] private float holdDuration;
-    [SerializeField] private float increaseDuration;
+
+    [SerializeField] private float increaseDuration, holdDuration;
+    [SerializeField] private float scalingTime;
+
+    private UITransformController uiTransformController;
+    private Coroutine corouBtn;
 
     private float holdTime, increaseTime;
     private bool isHolding, interactive;
 
     private void Start()
     {
-        generalItem = GetComponentInParent<GeneralItem>();
+        uiTransformController = UITransformController.Instance;
     }
 
     private void Update()
@@ -26,12 +31,38 @@ public class HoldGStatsButton : MonoBehaviour, IPointerDownHandler, IPointerUpHa
             holdTime += Time.deltaTime;
             if (holdTime >= holdDuration)
             {
-                increaseTime += Time.deltaTime;
+                increaseTime = Mathf.Min(increaseDuration, increaseTime + Time.deltaTime);
+                float half = increaseDuration / 2;
+                if (corouBtn == null)
+                {
+                    float start = 0.9f, end = 1f;
+                    float temp = end - start;
+                    float tempTime = increaseTime - half;
+                    float scaling, t;
+                    if (increaseTime <= half)
+                    {
+                        // scale down
+                        t = temp - tempTime * temp / half;
+                    }
+                    else
+                    {
+                        // scale up
+                        t = tempTime * temp / half;
+                    }
+                    scaling = t + start;
+                    rectTfm.localScale = Vector3.one * scaling;
+                }
+
                 if (increaseTime >= increaseDuration)
                 {
                     // increase gold faster
                     generalItem.EnhanceItem();
                     increaseTime = 0;
+                    if (corouBtn != null)
+                    {
+                        StopCoroutine(corouBtn);
+                        corouBtn = null;
+                    }
                 }
             }
         }
@@ -46,6 +77,13 @@ public class HoldGStatsButton : MonoBehaviour, IPointerDownHandler, IPointerUpHa
         if (!interactive)
             return;
 
+        if (corouBtn != null)
+        {
+            StopCoroutine(corouBtn);
+            corouBtn = null;
+        }
+
+        corouBtn = StartCoroutine(uiTransformController.IEScalingRect(rectTfm, rectTfm.localScale, Vector2.one * 0.9f, scalingTime, LerpType.Liner));
         isHolding = true;
         holdTime = 0;
         increaseTime = 0;
@@ -60,10 +98,17 @@ public class HoldGStatsButton : MonoBehaviour, IPointerDownHandler, IPointerUpHa
         if (!interactive)
             return;
 
+        if (corouBtn != null)
+        {
+            StopCoroutine(corouBtn);
+            corouBtn = null;
+            corouBtn = StartCoroutine(uiTransformController.IEScalingRect(rectTfm, rectTfm.localScale, Vector2.one, scalingTime, LerpType.Liner));
+        }
+
         isHolding = false;
         if (holdTime < holdDuration) // increase gold by 1 time if user doesn't hold button for long
         {
-            generalItem.EnhanceItem();
+            generalItem.EnhanceItem();  
         }
     }
 
@@ -72,7 +117,11 @@ public class HoldGStatsButton : MonoBehaviour, IPointerDownHandler, IPointerUpHa
         interactive = isActive;
         imgBtn.color = !isActive ? Color.red : Color.green;
         if (isHolding && !isActive)
+        {
             isHolding = false;
+            rectTfm.localScale = Vector2.one;
+            corouBtn = null;
+        }
     }
 
     public void SetMaxLv()
