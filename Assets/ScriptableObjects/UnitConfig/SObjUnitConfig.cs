aa0058bee342291cff5d06aebc8a94936286d505
Tiber <trantien.developer@gmail.com>
2024-04-19 19:45:28 +0700

    create unit scobj

---
+++ Assets/ScriptableObjects/UnitConfig/SObjUnitConfig.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "1000^n", menuName = "ScriptableObjects/SObjUnitConfig")]
+public class SObjUnitConfig : ScriptableObject
+{
+    public string unit;
+}
